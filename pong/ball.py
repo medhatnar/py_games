dc7644b13eab4cdf0c759841041459c35df63122
narmander <narminshahin@gmail.com>
2020-03-12 12:48:37 -0400

    ball moved into its own class

--- pong/ball.py
+++ pong/ball.py
@@ -3,4 +3,11 @@ import os
 
 class Ball:
     def __init__(self):
-        
+        ball = turtle.Turtle() 
+        ball.speed(0) 
+        ball.shape("square")
+        ball.color("white")
+        ball.penup()
+        ball.goto(0, 0)
+        ball.dx = 2
+        ball.dy = 2
--- pong/pong.py
+++ pong/pong.py
@@ -34,17 +34,6 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-# Ball
-ball = turtle
-ball = turtle.Turtle() 
-ball.speed(0) 
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
-
 # Pen
 pen = turtle.Turtle()
 pen.speed(0)
